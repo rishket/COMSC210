388070d183e45e6841a18ae38c5da2dddd5ecfa9
rishket <rishi.k.vadrevu@gmail.com>
2025-09-21 20:34:04 -0700

    Added Struct, constructor/destructor, and printStudent()

--- 210-lab-11/main.cpp
+++ 210-lab-11/main.cpp
@@ -3,7 +3,41 @@
 
 using namespace std;
 
-int main(){
-    
-    return 0;
+// Struct to represent a Student
+struct Student
+{
+    std::string name; // Student's name
+    int id;           // Student's ID
+    double *grades;   // Dynamic array to store grades
+    int numGrades;    // Number of grades
+
+    // Constructor to initialize a student with a name, id, and number of grades
+    Student(const std::string &n, int i, int numG) : name(n), id(i), numGrades(numG)
+    {
+        grades = new double[numGrades]; // Allocate dynamic array for grades
+    }
+    // Destructor to free the dynamic array
+    ~Student()
+    {
+        delete[] grades;
+    }
+};
+
+// Function to print a student's info and grades
+void printStudent(const Student &s)
+{
+    std::cout << "Name: " << s.name << ", ID: " << s.id << "\nGrades: ";
+    for (int i = 0; i < s.numGrades; ++i)
+    {
+        std::cout << s.grades[i];
+        if (i < s.numGrades - 1)
+            std::cout << ", ";
+    }
+    std::cout << std::endl;
 }
+
+    int main()
+    {
+
+        return 0;
+    }
