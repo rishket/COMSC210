eadd013b454e05ca7bf2b52b27c6d92234c9c40a
Rishi <66543602+rishket@users.noreply.github.com>
2025-09-28 21:38:49 -0700

    create getters and setters

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -4,16 +4,35 @@
 #include <vector>
 using namespace std;
 
-class Movie {
+class Movie
+{
 private:
     string title;
     int yearReleased;
     string screenWriter;
 
 public:
-  // Constructors
-  Movie() : yearReleased(0) {}
-Movie(const string& t, int y, const string& w)
-  : title(t), yearReleased(y), screenWriter(w) {}
+    // Constructors
+    Movie() : yearReleased(0) {}
+    Movie(const string &t, int y, const string &w)
+        : title(t), yearReleased(y), screenWriter(w) {}
 
+    // Setters
+    void setTitle(const string &t) { title = t; }
+    void setYearReleased(int y) { yearReleased = y; }
+    void setScreenWriter(const string &w) { screenWriter = w; }
 
+    // Getters
+    string getTitle() const { return title; }
+    int getYearReleased() const { return yearReleased; }
+    string getScreenWriter() const { return screenWriter; }
+
+    // Print method
+    void print() const
+    {
+        cout << "Movie: " << screenWriter << endl;
+        cout << "    Year released: " << yearReleased << endl;
+        cout << "    Screenwriter: " << title << endl;
+        cout << endl;
+    }
+};
