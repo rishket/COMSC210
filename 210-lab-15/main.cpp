d79d98aaed82aa52f1e33ad53354d072f3465b46
Rishi <66543602+rishket@users.noreply.github.com>
2025-09-28 21:22:04 -0700

    Create constructors

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Movie {
+private:
+    string title;
+    int yearReleased;
+    string screenWriter;
+
+public:
+  // Constructors
+  Movie() : yearReleased(0) {}
+Movie(const string& t, int y, const string& w)
+  : title(t), yearReleased(y), screenWriter(w) {}
+
 
